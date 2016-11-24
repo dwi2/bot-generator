277aa667bb525c5b807984e73a0031e6fc8a884e
Sherman Chen <sherman.chens@gmail.com>
2016-11-24 22:36:29 +0900

    Revert "Add body-parser as picker middleware."
    This reverts commit 9fe7a1bf6fc1673d346d5ce1b51c434b5c165656.

--- server/main.js
+++ server/main.js
@@ -1,13 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-
 Meteor.startup(() => {
   // code to run on server at startup
-  var bodyParser = Meteor.npmRequire('body-parser');
-  Picker.middleware(bodyParser.json());
-
-  Picker.route('/webhook', function(params, req, res, next) {
-    console.log(req.body);
-    res.end();
-  });
 });
